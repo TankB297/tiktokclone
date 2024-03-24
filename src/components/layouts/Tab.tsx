57f75a4d8d0dc8208db95ee9f59f4c944489051b
Anthony Nguyen <tankjerry511@gmail.com>
2024-03-24 23:18:52 +0700

    Update fontWeight for tabs

--- src/components/layouts/Tab.tsx
+++ src/components/layouts/Tab.tsx
@@ -9,7 +9,7 @@ export const Tab = ({ IconName, label }: Props) => {
   return (
     <div className="flex flex-row items-center hover:bg-gray-500 p-3 pl-4">
       <IconName className="text-white" style={{ fontSize: "30px" }} />
-      <p className="font-bold text-xl ml-2 text-white">{label}</p>
+      <p className="font-medium text-xl ml-2 text-white">{label}</p>
     </div>
   );
 };
