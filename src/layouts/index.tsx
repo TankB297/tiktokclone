669a616ffa2f21f9ae2eb99b935444ec7f268050
Anthony Nguyen <tankjerry511@gmail.com>
2024-03-24 19:31:23 +0700

    Update left menu

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import tiktokLogo from "../assets/tiktok-logo.svg";
+import tiktokReward from "../assets/tiktok-reward.png";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import AddIcon from "@mui/icons-material/Add";
 import { LeftMenu } from "../components/layouts/LeftMenu.tsx";
@@ -42,13 +43,73 @@ const Layouts = (props: Props) => {
         </div>
       </div>
       <div className="contents">
-        <div className="bg-black w-1/6">
+        <div className="bg-black w-[18%]">
           <LeftMenu />
-          <p className="text-gray-500 ml-4 mr-3 mt-4">
-            Log in to follow creators, like videos, and view comments.
-          </p>
+          <div className="ml-4 pr-5 mt-4">
+            <p className="text-gray-500 font-medium">
+              Log in to follow creators, like videos, and view comments.
+            </p>
+          </div>
           <div className="bg-gray-800 p-3 border-2 border-red-500 mt-5 w-[85%] m-auto">
             <p className="text-red-500 text-center text-lg">Log in</p>
+          </div>
+          <div className="py-5 border-t-2 border-t-gray-800 mt-5 w-[85%] m-auto relative">
+            <img
+              src={tiktokReward}
+              className="w-[100%] m-auto"
+              alt="tiktok-reward"
+            />
+            <div className="absolute px-5 top-6 left-7 w-full">
+              <p className="text-white font-bold text-sm">
+                Create TikTok effects, get a reward
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row items-center gap-x-1.5 pl-4">
+            <a
+              href="/"
+              className="no-underline text-gray-500 font-extrabold text-[12px] hover:underline"
+            >
+              About
+            </a>
+            <a
+              href="/"
+              className="no-underline text-gray-500 font-extrabold text-[12px] hover:underline"
+            >
+              Newsroom
+            </a>
+            <a
+              href="/"
+              className="no-underline text-gray-500 font-extrabold text-[12px] hover:underline"
+            >
+              Contact
+            </a>
+            <a
+              href="/"
+              className="no-underline text-gray-500 font-extrabold text-[12px] hover:underline"
+            >
+              Careers
+            </a>
+          </div>
+          <div className="items-center pl-4 gap-1.5">
+            <a
+              href="/"
+              className="no-underline text-gray-500 font-extrabold text-[12px] hover:underline"
+            >
+              TikTok for Good
+            </a>
+            <a
+              href="/"
+              className="no-underline text-gray-500 font-extrabold text-[12px] hover:underline"
+            >
+              Advertise
+            </a>
+            <a
+              href="/"
+              className="no-underline text-gray-500 font-extrabold text-[12px] hover:underline"
+            >
+              TikTok LIVE Creator Network
+            </a>
           </div>
         </div>
         <div>{children}</div>
